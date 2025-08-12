47204e065782a3209ccc9f72874fa5733d4213c6
malay1410 <malay1410@gmail.com>
2025-08-12 11:26:28 -0400

    jest.config.js changes

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@
 export default {
   testEnvironment: "node",
   transform: {}, // no Babel
-  extensionsToTreatAsEsm: [".js"],
   moduleFileExtensions: ["js", "json"],
   // optional if you keep tests under tests/:
   // testMatch: ['**/tests/**/*.test.js'],
